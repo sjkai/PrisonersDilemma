fa578ba9cd10279c8aa09c4e962a4b1926d46472
Jbenefield14 <Jbenefield17@middleburgacademy.org>
2016-03-03 08:45:02 -0500

    Update player12.py

--- player12.py
+++ player12.py
@@ -12,7 +12,7 @@ class Player12:
    #Defining functions for each child(instance) of class Car
    def strategy(self,history, opponent_history, score, opponent_score, getting_team_name):
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'no name available'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
